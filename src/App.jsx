78770676a36a5a76e8bcfbbe14ea19d3a0252db8
Jose-M-Azevedo <jmariaqta@gmail.com>
2025-01-23 17:26:23 +0000

    Finished app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header.jsx";
-// import Signup from "./components/Signup.jsx";
+import Signup from "./components/Signup.jsx";
 import StateLogin from "./components/StateLogin.jsx";
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
       <Header />
       <main>
         <StateLogin />
+        <Signup />
       </main>
     </>
   );
